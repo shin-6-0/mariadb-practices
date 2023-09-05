b7450b0dea2a1826fa63d278012d3d8535b1acc3
shin-6-0 <syk6715@naver.com>
2023-09-05 18:48:22 +0900

    BookMall Book Test완료

--- jdbc-practices/src/main/java/bookmall/dao/BookDao.java
+++ jdbc-practices/src/main/java/bookmall/dao/BookDao.java
@@ -1,5 +1,172 @@
 package bookmall.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import bookmall.vo.BookVo;
+import bookmall.vo.CategoryVo;
+
 public class BookDao {
+
+	public List<BookVo> selectBookAll() {
+		List<BookVo> result = new ArrayList<>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn=getConnection();
+
+			//3. SQL 준비
+			String sql = "select title,price from book";
+			pstmt = conn.prepareStatement(sql);
+			
+			//4. binding
+			
+			//5. SQL 실행
+			rs = pstmt.executeQuery();
+			
+			//6. 결과 처리
+			while(rs.next()) {
+				String title = rs.getString(1);
+				Long price = rs.getLong(2);
+
+				BookVo vo = new BookVo();
+				vo.setTitle(title);
+				vo.setPrice(price);
+				
+				result.add(vo);
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				// 6. 자원정리
+				if(rs != null) {
+					rs.close();
+				}
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		
+		}
+		return result;
+	}
+	
+	public boolean insert(BookVo vo) {
+		boolean result = false;
+
+		Connection conn = null;
+		PreparedStatement pstmtBefore = null;
+		PreparedStatement pstmt = null;
+
+		ResultSet rs = null;
+
+		try {
+			conn=getConnection();
+
+			//3. SQL 준비
+			String chkCategorySql="select no from category where name = '"
+					+vo.getCategoryName()+"' ";
+			pstmtBefore = conn.prepareStatement(chkCategorySql);
+			rs = pstmtBefore.executeQuery();
+			Long categoryNo = 0L;
+			while(rs.next()) {
+				categoryNo = rs.getLong(1);
+			}
+		
+			String sql = "insert into"
+			+" book (title,price,category_no) values (?,?,?) ";
+			pstmt = conn.prepareStatement(sql);
+			//4. binding
+			pstmt.setString(1, vo.getTitle());
+			pstmt.setLong(2, vo.getPrice());
+			pstmt.setLong(3, categoryNo);
+			int count = pstmt.executeUpdate();
+			result = count == 1;
+						
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		
+		}
+		return result;
+	}
+	
+	public boolean updateBookPrice(String title, Long price) {
+		boolean result = false;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn=getConnection();
+
+			//3. SQL 준비
+			String sql = "update book"
+					+ " set price = ?"
+					+ " where title = ? ";
+			pstmt = conn.prepareStatement(sql);
+			//4. binding
+			pstmt.setLong(1, price);
+			pstmt.setString(2, title);
+
+			int count = pstmt.executeUpdate();
+			result = count == 1;
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		
+		}
+		return result;
+	}
+	
+	private Connection getConnection() throws SQLException {
+		Connection conn=null;
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			String url = "jdbc:mariadb://192.168.0.13:3307/bookmall?charset=utf8";
+			conn = DriverManager.getConnection(url, "bookmall", "bookmall");
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		} 
+		return conn;
+	}
+
+
+
+
 
 }
--- jdbc-practices/src/main/java/bookmall/dao/test/BookDaoTest.java
+++ jdbc-practices/src/main/java/bookmall/dao/test/BookDaoTest.java
@@ -1,17 +1,56 @@
 package bookmall.dao.test;
 
+import java.util.List;
+
 import bookmall.dao.BookDao;
+import bookmall.dao.CategoryDao;
+import bookmall.dao.MemberDao;
+import bookmall.vo.BookVo;
+import bookmall.vo.MemberVo;
 
 public class BookDaoTest {
 	public static void main(String[] args) {
-		insertTest();
-		findAllTest();
+		BookVo vo = new BookVo();
+		vo.setTitle("JAVA의 정석");
+		vo.setPrice(55000L);
+		vo.setCategoryName("IT서적");
+
+		selectBookAll();
+		insertBook(vo);
+		selectBookAll();
+		updateBookPrice("JAVA의 정석",49500L);
+		selectBookAll();
+
+		/*selectBook();
+		deleteBook("신유경","cheerup313@naver.com");
+		selectBook();
+		updatePW("010-1111-2222","choonsik@gmail.com","chunchun2","cnstlr4321");
+		selectBook();*/
 	}
 
-	private static void insertTest() {
-		BookDao dao = new BookDao();
-		
-		dao.insert(bookVo);
-		dao.insert(bookVo);
+	private static void updateBookPrice(String title, Long price) {
+		boolean updatePrice = new BookDao().updateBookPrice(title,price);
+		if(updatePrice) {
+			System.out.println(">> 가격 변경 (Update) 실행 : 제목 = '"+title+"'인 책의 가격을 "
+					+ price +"원으로 수정");
+		}
+	}
+
+	private static void selectBookAll() {
+		List<BookVo> list = new BookDao().selectBookAll();
+		System.out.println("****************Book list***************");
+		for (BookVo vo : list) {
+			System.out.println("제목 : " + vo.getTitle() 
+							+ "\t 가격 : " + vo.getPrice());
+		}
+		System.out.println("****************************************");
+	}
+
+	private static void insertBook(BookVo vo) {
+		boolean insertTrue =new BookDao().insert(vo);
+		if(insertTrue) {
+			System.out.println(">> Insert 실행 : 제목 = '" + vo.getTitle() 
+			+ "', 가격 = " + vo.getPrice()+"원, 카테고리 = '"+vo.getCategoryName()+"'인 책 추가");
+		}
 	}
 }
