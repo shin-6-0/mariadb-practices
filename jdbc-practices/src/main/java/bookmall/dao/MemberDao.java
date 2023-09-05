4c70a2a4ed4072d55072d32b467c8cbf69672f80
shin-6-0 <syk6715@naver.com>
2023-09-05 18:47:18 +0900

    BookMall Member Test완료

--- jdbc-practices/src/main/java/bookmall/dao/MemberDao.java
+++ jdbc-practices/src/main/java/bookmall/dao/MemberDao.java
@@ -1,5 +1,194 @@
 package bookmall.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import bookmall.vo.MemberVo;
+
 public class MemberDao {
+
+	public List<MemberVo> select() {
+		List<MemberVo> result = new ArrayList<>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn=getConnection();
+
+			//3. SQL 준비
+			String sql = "select * from member";
+			pstmt = conn.prepareStatement(sql);
+			
+			//4. binding
+			
+			//5. SQL 실행
+			rs = pstmt.executeQuery();
+			
+			//6. 결과 처리
+			while(rs.next()) {
+				Long no = rs.getLong(1);
+				String name = rs.getString(2);
+				String phone = rs.getString(3);
+				String email = rs.getString(4);
+				String password = rs.getString(5);
+				
+				MemberVo vo = new MemberVo();
+				vo.setNo(no);
+				vo.setName(name);
+				vo.setEmail(email);
+				vo.setPassword(password);
+				vo.setPhone(phone);
+				
+				
+				result.add(vo);
+				
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				// 6. 자원정리
+				if(rs != null) {
+					rs.close();
+				}
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		
+		}
+		return result;
+	}
+
+	public void insert(MemberVo vo) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn=getConnection();
+
+			//3. SQL 준비
+			String sql = "insert into"
+			+" member (name, phone, email, password )"
+			+ " values ( ? , ? , ? , ? ) ";
+			pstmt = conn.prepareStatement(sql);
+			//4. binding
+			pstmt.setString(1, vo.getName());
+			pstmt.setString(2, vo.getPhone());
+			pstmt.setString(3, vo.getEmail());
+			pstmt.setString(4, vo.getPassword());
+			pstmt.executeQuery();
+						
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		
+		}
+	}
+
+	public void updatePW(String phone, String email,String pwBefore, String pwAfter) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn=getConnection();
+
+			//3. SQL 준비
+			String sql = "update member "
+					+ " set password = ?"
+					+ "where phone = ? and email = ? and password = ?";
+			pstmt = conn.prepareStatement(sql);
+			//4. binding
+			pstmt.setString(1, pwAfter);
+			pstmt.setString(2, phone);
+			pstmt.setString(3, email);
+			pstmt.setString(4, pwBefore);
+
+			pstmt.executeQuery();
+						
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		
+		}
+	}
+	
+	public void delete(String MemberName, String email) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn=getConnection();
+
+			//3. SQL 준비
+			String sql = "delete from member where name = ?"
+					+ " and email = ? ";
+			pstmt = conn.prepareStatement(sql);
+			//4. binding
+			pstmt.setString(1, MemberName);
+			pstmt.setString(2, email);
+			pstmt.executeQuery();
+						
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		
+		}
+	}
+	
+	private Connection getConnection() throws SQLException {
+		Connection conn=null;
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			String url = "jdbc:mariadb://192.168.0.13:3307/bookmall?charset=utf8";
+			conn = DriverManager.getConnection(url, "bookmall", "bookmall");
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		} 
+		return conn;
+	}
+
 
 }
--- jdbc-practices/src/main/java/bookmall/dao/test/MemberDaoTest.java
+++ jdbc-practices/src/main/java/bookmall/dao/test/MemberDaoTest.java
@@ -1,15 +1,57 @@
 package bookmall.dao.test;
 
+import java.util.List;
+
+import bookmall.dao.BookDao;
+import bookmall.dao.MemberDao;
+import bookmall.dao.MemberDao;
+import bookmall.vo.MemberVo;
+import bookmall.vo.MemberVo;
+
 public class MemberDaoTest {
 	public static void main(String[] args) {
-		insertTest();
-		findAllTest();
+		MemberVo vo = new MemberVo();
+		vo.setName("신유경");
+		vo.setEmail("cheerup313@naver.com");
+		vo.setPassword("shinyukyung313");
+		vo.setPhone("010-1234-4321");
+
+		selectMember();
+		insertMember(vo);
+		selectMember();
+		deleteMember("신유경","cheerup313@naver.com");
+		selectMember();
+		updatePW("010-1111-2222","choonsik@gmail.com","chunchun2","cnstlr4321");
+		selectMember();
+		/*
+		 * insertMember(vo); selectMember(); deleteMember("춘식이","choonsik@gmail.com");
+		 * selectMember();
+		 */
 	}
 
-	private static void insertTest() {
-		BookDao dao = new BookDao()
-		
-		dao.insert(bookVo);
-		dao.insert(bookVo);
+	private static void updatePW(String phone, String email,String pwBefore, String pwAfter) {
+		System.out.println(">> 회원비밀번호 변경(Update) 실행 : email이 '" + email + "인 회원의 비밀번호를 '" + pwBefore + "'에서 "+pwAfter+"로 변경");
+		new MemberDao().updatePW(phone,email,pwBefore,pwAfter);
+	}
+
+	private static void deleteMember(String MemberName, String email) {
+		System.out.println(">> 회원탈퇴(Delete) 실행 : name = '" + MemberName + ", email = '" + email + "'인 멤버");
+		new MemberDao().delete(MemberName,email);
+	}
+
+	private static void selectMember() {
+		List<MemberVo> list = new MemberDao().select();
+		System.out.println("********************Member list*******************");
+		for (MemberVo vo : list) {
+			System.out.println("No : " + vo.getNo() + "\t 이름 : " + vo.getName() + "\t 연락처 : " + vo.getPhone()
+					+ "\t 이메일 : " + vo.getEmail() + "\t 비밀번호 : " + vo.getPassword());
+		}
+		System.out.println("**************************************************");
+	}
+
+	private static void insertMember(MemberVo vo) {
+		new MemberDao().insert(vo);
+		System.out.println(">> 회원가입(Insert) 실행 : 이름 = " + vo.getName() + ", 연락처 : " + vo.getPhone()
+			+ ", 이메일 : " + vo.getEmail() + ", 비밀번호 : " + vo.getPassword()+ "인 멤버 추가");
 	}
 }
